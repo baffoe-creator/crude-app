58e202c236834f0f871112cd618a0fc06efca880
baffoe-creator <baffoekuuku9@gmail.com>
2025-07-24 16:53:02 +0000

    minor updates

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'http://localhost:5173'],
+  origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
